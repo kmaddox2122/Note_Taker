3c2615d20fe08cfa01dd85547b63be66199a0a75
Katie Maddox <kmaddox2122@gmail.com>
2022-10-28 21:59:17 -0500

    finished post route

--- server.js
+++ server.js
@@ -56,17 +56,37 @@ app.post('/api/notes', (req, res) => {
   console.info(`${req.method} request received to add a note`);
 
   // Destructuring assignment for the items in req.body
-  const { product, review, username } = req.body;
+  const { title, text } = req.body;
 
-  // If all the required properties are present         //syntax error and need to rename vars, etc.
-  if (product && review && username) {
+  // If all the required properties are present        
+  if (title && text) {
     // Variable for the object we will save
-    const newReview = {
-      product,
-      review,
-      username,
-      review_id: uuid(),
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
     };
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+        // res.json(parsedNotes);
+        // Add a new review
+        parsedNotes.push(newNote);
+    
+        // Write updated reviews back to the file
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? res.json(writeErr)
+              : res.json(parsedNotes)
+        );
+      }
+    });
   };
 });
 
