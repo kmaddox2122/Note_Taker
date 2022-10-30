0dc74a56917cfcad4381fd80e2852b302a0ec57b
Katie Maddox <kmaddox2122@gmail.com>
2022-10-30 11:28:43 -0500

    pending delete request for api notes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 
 // Helper method for generating unique ids
-const uuid = require('./helpers/uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const PORT = process.env.PORT || 3001;
 
@@ -35,18 +35,6 @@ fs.readFile('./db/db.json', 'utf8', (err, data) => {
     // Convert string into JSON object
     const parsedNotes = JSON.parse(data);
     res.json(parsedNotes);
-    // Add a new review
-    // parsedNotes.push(newNote);
-
-    // // Write updated reviews back to the file
-    // fs.writeFile(
-    //   './db/db.json',
-    //   JSON.stringify(parsedNotes, null, 4),
-    //   (writeErr) =>
-    //     writeErr
-    //       ? console.error(writeErr)
-    //       : console.info('Successfully updated notes!')
-    // );
   }
 });
 });
@@ -64,7 +52,7 @@ app.post('/api/notes', (req, res) => {
     const newNote = {
       title,
       text,
-      id: uuid(),
+      id: uuidv4(),
     };
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if (err) {
@@ -73,7 +61,7 @@ app.post('/api/notes', (req, res) => {
         // Convert string into JSON object
         const parsedNotes = JSON.parse(data);
         // res.json(parsedNotes);
-        // Add a new review
+        // Add a new note
         parsedNotes.push(newNote);
     
         // Write updated reviews back to the file
@@ -89,6 +77,24 @@ app.post('/api/notes', (req, res) => {
     });
   };
 });
+
+// DELETE request for api notes
+app.delete('/api/notes/:id', (req, res) => {
+
+  // obtain existing notes
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      // Convert string into JSON object
+      const parsedNotes = JSON.parse(data);
+      res.json(parsedNotes);
+    // if ()
+    //   res.json({msg: "Note Deleted"})
+    //   parsedNotes.filter
+    }
+  });
+  });
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
